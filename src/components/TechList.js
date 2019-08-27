cafe53361b10ec2005d25c3d43662e7329457877
Erik Garces Matos <erik_senac@hotmail.com>
2019-08-27 09:35:02 -0300

    Class components

---
+++ src/components/TechList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class TechList extends Component {
+  state = {
+    techs: ["NodeJS", "RecatJS", "Raect Native"]
+  };
+
+  render() {
+    return (
+      <ul>
+        <li>NodeJS</li>
+        <li>ReactJS</li>
+        <li>React Native</li>
+      </ul>
+    );
+  }
+}
+
+export default TechList;
